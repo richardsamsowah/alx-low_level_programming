6ba631cc044887c5264c87dd6058a39732088018
Richard <samsowah123@gmail.com>
2023-02-16 21:18:16 -0800

    Task 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * * main- prints a string followed by a new line
- * * Return: 0
- * */
+ * main- prints a string followed by a new line
+ * Return: 0
+ */
 int main(void)
 {
 	        printf("with proper grammar, but the outcome is a piece of art,\n");
